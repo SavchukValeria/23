f089d416b9eabf21a1c7c1c0514e55e908be337d
SavchukValeria <V.Savchuk_FIT_12_21_B_d@knute.edu.ua>
2022-10-07 16:35:05 +0300

    1
    1

---
+++ Lad3/Program/three/First.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program.Three
+{
+    internal class First: Person
+    {
+        public First(string N, int A)
+        {
+            Name = N;
+            Age = A;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("First class");
+            Console.WriteLine($"Name = {Name}");
+            Console.WriteLine($"Age = {Age}");
+        }
+    }
+}
---
+++ Lad3/Program/two/A.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program.Two
+{
+    public class A
+    {
+        public B Bobject { get; set; }
+        public B Bobject1 { get; set; }
+        public B Bobject2 { get; set; }
+        public A(B obj0, B obj1, B obj2)
+        {
+            Bobject = obj0;
+            Bobject1 = obj1;
+            Bobject2 = obj2;
+        }
+    }
+}
